9ad89ab6b6cc16c5ff4d653da7dea6a5d41a9de0
Arnaud PEETERS <16266@ecam.be>
2017-11-15 11:44:05 +0100

    4eme labo

--- controllers/app.php
+++ controllers/app.php
@@ -1,16 +1,40 @@
 <?php
 
-class app
+class App
 {
 	public function route()
 	{
-		$this->home();
+		include_once 'models/Reservation.php';
+		
+		if (isset($_POST['new'])) {
+            $this->newR();
+		} elseif (isset($_POST['stage1'])) {
+            $this->stage1();
+        } elseif (isset($_POST['stage2'])) {
+            $this->stage2();
+        } else {
+			$this->home();
+		}
 	}
 	
-	public function home()
+	private function home()
 	{
-		include'views/reservation-form-0.php';
+		include'views/mainpage.php';
 	}
 	
+	private function newR()
+	{
+		include'views/reservation-form.php';
+	}
+	
+	private function stage1()
+	{
+		include'views/registration-form.php';
+	}
+	
+	private function stage2()
+	{
+		include'views/payment-form.php';
+	}
 }
 		
